4077a3a449af2ceb372f42625a3ea90227882e8d
Elyse Burmester <burmesterelyse@gmail.com>
2019-09-06 19:49:38 +0000

    Automatically backed up by Learn

--- src/containers/MoviesPage.js
+++ src/containers/MoviesPage.js
@@ -4,16 +4,14 @@ import { Route } from 'react-router-dom';
 import MoviesList from '../components/MoviesList';
 import MovieShow from './MovieShow';
 
-const MoviesList = ({ movies }) => {
-  const renderMovies = Object.keys(movies).map(movieID =>
-    <Link key={movieID} to={`/movies/${movieID}`}>{movies[movieID].title}</Link>
-  );
- 
-  return (
-    <div>
-      {renderMovies}
-    </div>
-  );
-};
+const MoviesPage = ({ match, movies }) => (
+  <div>
+    <MoviesList movies={movies} />
+    <Route exact path={match.url} render={() => (
+      <h3>Please select a Movie from the list.</h3>
+    )}/>
+    <Route path={`${match.url}/:movieId`} component={MovieShow}/>
+  </div>
+)
 
 export default MoviesPage
